5007cf36ec9a4e18b4c2964f1a3536035ca0b2a3
krisstallino <krisstallino@gmail.com>
2021-07-09 13:31:45 +0300

    refixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-    long expected = 30;
 
-    long actual = service.calculate(amount, registered);
-    boolean passed = expected == actual;
-
-    System.out.println(passed);
+    long calculate = service.calculate(amount, registered);
+    System.out.println(calculate);
   }
 }
 
